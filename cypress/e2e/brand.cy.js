598b336a2137a0f459b3dfe9fe89ce7a34e119f3
nusratmehnaz <nusrat.mehnaz@vivasoftltd.com>
2023-01-10 19:59:12 +0600

    Update brand info

--- cypress/e2e/brand.cy.js
+++ cypress/e2e/brand.cy.js
@@ -41,3 +41,35 @@ describe("User can successfully create a brand", () => {
         cy.get(this.brand.toastMSG).should('have.text', 'Brand created successfully');
     });
 })
+
+describe('User can successfully update any brand', () => {
+
+    beforeEach(function () {
+        cy.fixture('/locators/brand.json').then(function (brand) {
+            this.brand = brand
+        })
+        cy.fixture('/data/new_brand.json').then(function (new_brand) {
+            this.new_brand = new_brand
+        })
+        cy.fixture('/data/update_brand_info.json').then(function (update_brand_info) {
+            this.update_brand_info = update_brand_info
+        })
+    })
+
+    it('Search and select the brand', function () {
+        cy.get(this.brand.searchBX).type(this.new_brand.brand_name)
+        cy.get(this.brand.selectBrand).click()
+        cy.wait(2000);
+    });
+
+    xit('Update the necessary informations', function () {
+        cy.get(this.brand.brandNameFLD).clear().type(this.update_brand_info.brand_name);
+        cy.get(this.brand.QRLabelFLD).clear().type(this.update_brand_info.QR_label);
+        cy.get(this.brand.QRurlFLD).clear().type(this.update_brand_info.QR_URL)
+    });
+
+    it('Click on Update button and verify the success toast message', function () {
+        cy.contains('Update').click();
+        cy.get(this.brand.toastMSG).should('have.text', 'Brand updated successfully');
+    });
+})
